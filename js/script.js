4b1ef7ece999c2022af9f505988171862e5e2df3
AriqAhmer <ariq_kabbo@yahoo.com>
2020-10-26 01:28:46 +0200

    changes made to js and css for min-width

--- js/script.js
+++ js/script.js
@@ -7,3 +7,8 @@ hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("open");
     container.classList.toggle("open");
 });
+
+var scale = 'scale(1)';
+document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
+document.body.style.msTransform =   scale;       // IE 9
+document.body.style.transform = scale;     // General
